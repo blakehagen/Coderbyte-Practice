fbac10e43846734a3e926dcc54d27639fe7bef0b
Blake Hagen <blakehagen@gmail.com>
2016-01-25 20:28:32 -0700

    added

--- practice.js
+++ practice.js
@@ -313,4 +313,19 @@ function looper(arr) {
 } 
 
 // // // // // // // // / /// // //
+// Write a sum method which will work properly when invoked using either syntax below.
 
+// console.log(sum(2,3));   // Outputs 5
+// console.log(sum(2)(3));  // Outputs 5
+
+function sum(x, y) {
+    if (y !== undefined) {
+        return x + y;
+    } else {
+        return function (y) {
+            return x + y;
+        };
+    }
+}
+
+// // // // // // // // // // // // // 
